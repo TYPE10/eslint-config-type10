7416b2cba720ecd06d1e267b20555d72e589fb6a
jquijino <jay.quijino@gmail.com>
2019-10-18 18:07:28 +0200

    Removes a few overrides

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 module.exports = {
     extends: ['airbnb-base'],
     rules: {
-        'no-param-reassign': 0,
-        'no-multi-assign': 0,
-        'comma-dangle': [2, 'only-multiline'],
         'max-len': [0, 120, 4],
-        indent: [2, 4],
-        indent: [2, 4, { SwitchCase: 1, VariableDeclarator: 1 }],
     }
 };
 
